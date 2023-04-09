983a923c3f0c7ac79a7acb08e7e2da060bc188f8
James-Bolt <gleb.frolow2013@gmail.com>
2023-04-09 19:52:42 +0300

    little fix

--- src/test/java/models/LimitResponse.java
+++ src/test/java/models/LimitResponse.java
@@ -1,7 +1,6 @@
 package models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
